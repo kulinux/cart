a4c1a66e17803fa154647ac706805bcc12c49396
Paco Benitez <paco.benitez.lazaro@gmail.com>
2019-08-19 20:50:22 +0200

    Cassandra Client

---
+++ play/app/services/CassandraService.scala
@@ -0,0 +1,92 @@
+package services
+
+import java.util.concurrent.{Executor, Executors}
+
+import com.datastax.driver.core.querybuilder.{QueryBuilder, Select}
+import com.datastax.driver.core.{Cluster, ResultSet, ResultSetFuture, Statement}
+import services.CassandraService.CassandraSku
+
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
+
+import scala.language.implicitConversions
+import play.api.libs.json._
+
+import scala.concurrent.duration._
+
+
+//@Singleton
+class CassandraService(implicit executionContext: ExecutionContext) {
+
+  implicit val session = new Cluster
+  .Builder()
+    .addContactPoints("localhost")
+    .withPort(9042)
+    .build()
+    .connect()
+
+  def getById(id: String): Future[CassandraSku] = {
+    val select = QueryBuilder.select()
+        .json()
+        .from("store", "skus")
+        .where( QueryBuilder.eq("code", id))
+    val res = session.executeAsync( select )
+
+    listenableFutureToFuture(res)
+      .map(buildCassandraSku)
+  }
+
+  def buildCassandraSku(rs: ResultSet): CassandraSku = {
+    val jsonStr = rs.one()
+      .getString("[json]")
+    val json : JsObject = Json.parse(jsonStr)
+      .asInstanceOf[JsObject]
+
+    val code = (json \ "code").get.as[String]
+    val attr : Map[String, String] =
+      json.fieldSet.map{case (key, value) => (key, value.as[String])}
+          .toMap
+
+
+    CassandraSku(code, attr)
+  }
+
+  implicit def listenableFutureToFuture[T](
+                                            listenableFuture: ListenableFuture[T]
+                                          ): Future[T] = {
+
+    val executor = new MyExecutor()
+
+    val promise = Promise[T]()
+    Futures.addCallback(listenableFuture, new FutureCallback[T] {
+      def onFailure(error: Throwable): Unit = {
+        promise.failure(error)
+        ()
+      }
+      def onSuccess(result: T): Unit = {
+        promise.success(result)
+        ()
+      }
+    }, executor)
+    promise.future
+  }
+}
+
+class MyExecutor(implicit ec: ExecutionContext) extends Executor {
+  override def execute(command: Runnable): Unit = ec.execute(command)
+}
+
+object CassandraService {
+  case class CassandraSku(id: String, attr: Map[String, String])
+}
+
+object FastTestCassandra extends App {
+  import ExecutionContext.Implicits.global
+  val cassandraService = new CassandraService()
+
+  val fut = cassandraService.getById("8410095506431")
+    .map(println)
+
+  Await.result(fut, 10 seconds)
+
+}
--- play/build.sbt
+++ play/build.sbt
@@ -12,10 +12,15 @@ libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.2
 libraryDependencies += "com.typesafe.play" %% "play-slick" % "3.0.0"
 libraryDependencies += "com.h2database" % "h2" % "1.4.192"
 
+libraryDependencies += "com.datastax.cassandra" % "cassandra-driver-core" % "3.7.1"
+
+
+
 val elastic4sVersion = "6.3.3"
 libraryDependencies ++= Seq(
   "com.sksamuel.elastic4s" %% "elastic4s-http" % elastic4sVersion
 )
+
 
 
 // Adds additional packages into Twirl
